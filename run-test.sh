c5e3ad2a55d7f5a1facd5a467ab1b3b94dfcdbbe
Leonid Shalupov <shalupov@diverse.org.ru>
2013-01-09 21:52:07 +0000

    use virtualenv to setup tests

--- run-test.sh
+++ run-test.sh
@@ -1,21 +1,61 @@
 #!/bin/sh
 
-cd test
+set -e
+
 export TEAMCITY_PROJECT_NAME=project_name
 
-run() {
-    NAME="$1"
-    CMD="$2"
-    
-    $CMD 2>&1 | sed 's#File "[^"]*/#File "#g' | sed 's#instance at 0x.*>#instance at 0x????????>#' | perl -pe 's/line \d+/line LINE/g' >test-$NAME.output.tmp 
-    diff -Nru test-$NAME.output.gold test-$NAME.output.tmp
-    if [ $? = 0 ]; then
-        rm -f test-$NAME.output.tmp
-    else
-        echo "$NAME FAILED"
-    fi
+tmp=`mktemp -d`
+trap "rm -rf $tmp" EXIT
+
+prepare_nose() {
+    easy_install -q "$dcache/nose-1.2.1.tar.gz"
 }
 
-run unittest "python test-unittest.py"
-run nose "nosetests test-nose.py"
-run pytest "py.test --teamcity test-pytest.py"
+prepare_pytest() {
+    easy_install -q "$dcache/py-1.4.12.zip"
+    easy_install -q "$dcache/pytest-2.3.4.zip"
+}
+
+run() {
+    name="$1"
+    prereq="$2"
+    cmd="$3"
+
+    venv="$tmp/$name"
+
+    rm -rf "$venv"
+    virtualenv -q --never-download --no-site-packages "$venv"
+
+    (
+    . "$venv/bin/activate"
+    easy_install -q "$egg"
+    [ -z "$prereq" ] || $prereq
+
+    cd test
+    $cmd 2>&1 | \
+        sed 's#File "[^"]*/#File "#g' | \
+        perl -pe 's/passed in \d+\.\d+ seconds/passed in X.XX seconds/g' | \
+        perl -pe 's/^platform .+$/platform SPEC/' | \
+        sed 's#instance at 0x.*>#instance at 0x????????>#' | \
+        perl -pe 's/line \d+/line LINE/g' \
+        >test-$name.output.tmp
+
+    diff -Nru test-$name.output.gold test-$name.output.tmp
+    if [ $? = 0 ]; then
+        rm -f test-$name.output.tmp
+    else
+        echo "$name FAILED"
+    fi
+    )
+}
+
+rm -rf dist
+python setup.py -q bdist_egg
+egg=$(echo $(pwd)/dist/*.egg)
+test -f "$egg"
+
+dcache="$(pwd)/eggs"
+
+run unittest "" "python test-unittest.py"
+run nose "prepare_nose" "nosetests test-nose.py"
+run pytest "prepare_pytest" "py.test --teamcity test-pytest.py"
