5b437e7eb3b18067545e691013c5494e331ec566
Leonid Shalupov <shalupov@diverse.org.ru>
2013-01-23 16:26:04 +0400

    fixes #3 Dynamically generated tests produce random suite names

--- teamcity/nose_report.py
+++ teamcity/nose_report.py
@@ -1,10 +1,8 @@
 # coding=utf-8
-import types
 import os
 
 from teamcity import is_running_under_teamcity
 from teamcity.unittestpy import TeamcityTestResult
-
 
 class TeamcityReport(TeamcityTestResult):
     name = 'teamcity-report'
@@ -20,12 +18,9 @@ class TeamcityReport(TeamcityTestResult):
         pass
 
     def getCtxName(self, ctx):
-        if isinstance(ctx, types.ModuleType):
-            return ctx.__name__
-        elif isinstance(ctx, type) or isinstance(ctx, types.ClassType) or isinstance(ctx, types.InstanceType):
-            return ctx.__module__ + '.' + ctx.__name__
-        else:
-            return str(ctx)
+        parent_name = (ctx.__module__ + ".") if hasattr(ctx, "__module__") else ""
+        name = ctx.__name__ if hasattr(ctx, "__name__") else str(ctx)
+        return parent_name + name 
 
     def setOutputStream(self, stream):
         self.output = stream
--- test/test-nose.py
+++ test/test-nose.py
@@ -1,5 +1,13 @@
 # coding=utf-8
 
+case_data = [1,2,3]
+
+def checkValue(value):
+    assert value == 2
+
+def test_cases():
+    for case in case_data:
+        yield checkValue, case
 
 class TestTeamcityMessages:
     def testPass(self):
