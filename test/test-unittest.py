bdc517e85096b0851cafb0cb229adffafbb45a9b
Jon Schlueter <jschlueter@navigationsolutions.com>
2013-06-27 09:06:55 -0400

    PEP8 whitespace cleanup in test-unittest.py

--- test/test-unittest.py
+++ test/test-unittest.py
@@ -24,6 +24,7 @@ class TestTeamcityMessages(unittest.TestCase):
     def testException(self):
         raise Exception("some exception")
 
+
 class StreamStub(object):
     def __init__(self):
         self.observed_output = ''
@@ -31,27 +32,33 @@ class StreamStub(object):
     def write(self, msg):
         self.observed_output += msg
 
+
 class TestMessages(unittest.TestCase):
     def test_no_properties(self):
         stream = StreamStub()
-        messages = TeamcityServiceMessages(output=stream, now=lambda: datetime.min)
+        messages = TeamcityServiceMessages(output=stream,
+                                           now=lambda: datetime.min)
         messages.message('dummyMessage')
-        assert stream.observed_output == "\n##teamcity[dummyMessage timestamp='0001-01-01T00:00']\n"
-
+        assert stream.observed_output == \
+            "\n##teamcity[dummyMessage timestamp='0001-01-01T00:00']\n"
 
     def test_one_property(self):
         stream = StreamStub()
-        messages = TeamcityServiceMessages(output=stream, now=lambda: datetime.min)
+        messages = TeamcityServiceMessages(output=stream,
+                                           now=lambda: datetime.min)
         messages.message('dummyMessage', fruit='apple')
-        assert stream.observed_output == "\n##teamcity[dummyMessage timestamp='0001-01-01T00:00' fruit='apple']\n"
-
+        assert stream.observed_output == "\n##teamcity[dummyMessage " + \
+            "timestamp='0001-01-01T00:00' fruit='apple']\n"
 
     def test_three_properties(self):
         stream = StreamStub()
-        messages = TeamcityServiceMessages(output=stream, now=lambda: datetime.min)
-        messages.message('dummyMessage', fruit='apple', meat='steak', pie='raspberry')
-        assert stream.observed_output ==\
-               "\n##teamcity[dummyMessage timestamp='0001-01-01T00:00' fruit='apple' meat='steak' pie='raspberry']\n"
+        messages = TeamcityServiceMessages(output=stream,
+                                           now=lambda: datetime.min)
+        messages.message('dummyMessage', fruit='apple', meat='steak',
+                         pie='raspberry')
+        assert stream.observed_output == "\n##teamcity[dummyMessage " + \
+            "timestamp='0001-01-01T00:00' fruit='apple' meat='steak' " + \
+            "pie='raspberry']\n"
 
 
 if __name__ == '__main__':
