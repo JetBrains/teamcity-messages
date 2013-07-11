718cc332c60d87b344c6b81dff62fc12d7b7589b
Leonid Bushuev <leonidos@gmail.com>
2013-07-11 19:26:12 +0200

    Report comparison failures correctly (own tests)

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 # coding=utf-8
-import atexit
 import os
 import sys
 import re
 import shutil
-import tempfile
 from os.path import join
+import traceback
+import tests
+
 
 eggs = os.path.abspath("test_support")
+test_name = 'Unnamed_Test'
+in_teamcity = False
+
 
 
 class Framework(object):
@@ -22,6 +26,10 @@ FRAMEWORKS = [
     Framework("nose", "1.2.1", ["nosetests", "-w", "nose_integration_tests"]),
     Framework("pytest", "2.3.4", ["py.test", "--teamcity", "test-pytest.py"]),
 ]
+
+
+def generate_test_name(fw_name, fw_version):
+    return "Framework.%s(%s)" % (fw_name, fw_version)
 
 
 def normalize_output(s):
@@ -53,28 +61,12 @@ def build_egg():
     return os.path.abspath(dist.dist_files[0][2])
 
 
-def runner():
-    temp = tempfile.mkdtemp()
-    atexit.register(shutil.rmtree, temp)
-
-    ok = True
-    for fw in FRAMEWORKS:
-        test = "Framework.%s(%s)" % (fw.name, fw.version)
-        tc_msg("##teamcity[testStarted name='%s']" % test)
-        try:
-            success = run(fw, temp)
-            if not success:
-                ok = False
-                tc_msg("##teamcity[testFailed name='%s']" % test)
-        finally:
-            tc_msg("##teamcity[testFinished name='%s']" % test)
-
-    return ok
-
 
 def tc_msg(msg):
     if os.environ.get('TEAMCITY_VERSION') is not None:
-        print(msg)
+        sys.stdout.write(msg)
+        sys.stdout.write('\n')
+        sys.stdout.flush()
 
 
 def find_script(venv, name):
@@ -85,24 +77,6 @@ def find_script(venv, name):
 
     raise RuntimeError("No " + name + " found in " + venv)
 
-
-def run(fw, temp):
-    venv = join(temp, "venv-" + fw.name)
-    clean_directory(venv)
-
-    from test_support import virtualenv
-
-    # virtualenv.logger = virtualenv.Logger([(virtualenv.Logger.DEBUG, sys.stdout)])
-    virtualenv.DISTRIBUTE_SETUP_PY = virtualenv.DISTRIBUTE_SETUP_PY.replace("0.6.28", "0.6.34")
-    virtualenv.create_environment(venv, use_distribute=True,
-                                  never_download=True, search_dirs=[eggs])
-
-    import subprocess
-
-    python = find_script(venv, "python")
-    rc = subprocess.call([python, __file__, fw.name, fw.version, venv])
-
-    return rc == 0
 
 
 def install_file(file):
@@ -117,12 +91,25 @@ def install_package(package):
     easy_install.main(["-q", "-Hnone", "-f", eggs, package])
 
 
-def in_venv(venv, framework):
+def escape_for_tc_message_value(expected_output):
+    return expected_output \
+            .replace('\r', '')  \
+            .replace('|',  '||') \
+            .replace('\n', '|n') \
+            .replace("'",  "|'") \
+            .replace('[',  '|[') \
+            .replace(']',  '|]')
+
+
+
+def run_test_internal(venv, framework):
     teamcity_messages_egg = build_egg()
     install_file(teamcity_messages_egg)
 
     if framework.version != "bundled":
         install_package(framework.name + "==" + framework.version)
+
+    global in_teamcity, test_name
 
     (cmd, args) = (framework.test_command[0], framework.test_command[1:])
     cmd = find_script(venv, cmd)
@@ -147,34 +134,68 @@ def in_venv(venv, framework):
 
     actual_output_file = file_prefix + ".tmp"
     if expected_output != output:
-        sys.stderr.write(
-            "ERROR Wrong output, check the differences between " + expected_output_file + " and " + actual_output_file + "\n")
-        open(actual_output_file, "w").write(output)
+        if in_teamcity:
+            expected_string = escape_for_tc_message_value(expected_output)
+            actual_string = escape_for_tc_message_value(output)
+            message = "##teamcity[testFailed type='comparisonFailure' name='%s' message='Output doesn|'t match the relative gold file. See the diff for detail.' expected='%s' actual='%s']\n" \
+                    % (test_name, expected_string, actual_string)
+            sys.stdout.write(message)
+            sys.stdout.flush()
+        else:
+            sys.stderr.write(
+                "ERROR Wrong output, check the differences between " + expected_output_file + " and " + actual_output_file + "\n")
+            open(actual_output_file, "w").write(output)
 
-        DIFF = "/usr/bin/diff"
-        if os.path.exists(DIFF):
-            subprocess.call([DIFF, "-u", expected_output_file, actual_output_file])
-        return False
+            DIFF = "/usr/bin/diff"
+            if os.path.exists(DIFF):
+                subprocess.call([DIFF, "-u", expected_output_file, actual_output_file])
+        return 1
     else:
         sys.stdout.write("OK\n")
         if os.path.isfile(actual_output_file):
             os.unlink(actual_output_file)
-        return True
+        return 2
+
+
+
+def run_one_test_in_venv(venv, framework):
+    """
+    Runs one test for the specified virtual env and framework.
+    Returns:
+        2 - the test passed successfully
+        1 - the test finished with non-matched output (test failure)
+        0 or an exception - the test not finished (probably a run-time error happened)
+    """
+    try:
+        return run_test_internal(venv, framework)
+    except:
+        err_name = escape_for_tc_message_value(sys.exc_info()[0].__name__)
+        trace = escape_for_tc_message_value(traceback.format_exc())
+        if in_teamcity:
+            tc_msg("##teamcity[testFailed name='%s' message='%s' details='%s']" % (test_name, err_name, trace))
+        return 0
+
+
+
+
+def run_one_test(args):
+    (name, version, venv) = args
+    global test_name, in_teamcity
+    test_name = generate_test_name(name, version)
+    in_teamcity = os.environ.get('TEAMCITY_VERSION') is not None
+    success = 0
+    for fw in FRAMEWORKS:
+        if fw.name == name and fw.version == version:
+            success = run_one_test_in_venv(venv, fw)
+            break
+    sys.exit(success)
 
 
 def main(args):
-    success = False
-
-    if len(args) == 0:
-        success = runner()
+    if (len(args) == 0):
+        tests.main()
     else:
-        (name, version, venv) = args
-
-        for fw in FRAMEWORKS:
-            if fw.name == name and fw.version == version:
-                success = in_venv(venv, fw)
-
-    sys.exit(0 if success else 1)
+        run_one_test(args)
 
 
 if __name__ == "__main__":
