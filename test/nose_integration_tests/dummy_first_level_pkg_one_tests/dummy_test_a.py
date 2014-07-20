495fb19244a19373ca0041359a69dfed57b181d8
Leonid Shalupov <shalupov@diverse.org.ru>
2014-07-20 19:04:49 +0200

    fix tests under python 2.6

--- test/nose_integration_tests/dummy_first_level_pkg_one_tests/dummy_test_a.py
+++ test/nose_integration_tests/dummy_first_level_pkg_one_tests/dummy_test_a.py
@@ -12,7 +12,7 @@ def test_with_generator():
 
 
 def do_something(i):
-    assert True, "do something number: {}".format(i)
+    assert True, "do something number: {0}".format(i)
 
 
 def test_with_generator_and_pydoc():
@@ -20,7 +20,7 @@ def test_with_generator_and_pydoc():
     Tests that have pydoc are cool
     """
     for i in range(0, 5):
-        @attr(description="Do not worry, be happy {}".format(i))
+        @attr(description="Do not worry, be happy {0}".format(i))
         def wrapper(*args, **kwargs):
             do_something_else(*args, **kwargs)
 
@@ -31,7 +31,7 @@ def do_something_else(i):
     """
     Let's do something else instead.
     """
-    assert True, "do something else number: {}".format(i)
+    assert True, "do something else number: {0}".format(i)
 
 
 @attr(description="generators with pydoc and descriptions are the most funky")
@@ -40,7 +40,7 @@ def test_with_generator_and_description_and_pydoc():
     Tests that have pydoc are cool
     """
     for i in range(0, 5):
-        @attr(description="Do not worry, be funky {}".format(i))
+        @attr(description="Do not worry, be funky {0}".format(i))
         def wrapper(*args, **kwargs):
             do_something_funky(*args, **kwargs)
 
@@ -52,9 +52,9 @@ def do_something_funky(i):
     Let's do something funky instead.
     """
     if i == 1:
-        assert False, "i do not want to be funky just yet {}".format(i)
+        assert False, "i do not want to be funky just yet {0}".format(i)
     elif i == 2:
         evil_non_existent_method()
     else:
-        assert True, "do something funky number: {}".format(i)
+        assert True, "do something funky number: {0}".format(i)
 
