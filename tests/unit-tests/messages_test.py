b886db48e635897dac0e6f292c65a00d9e3a4ef9
Leonid Shalupov <shalupov@diverse.org.ru>
2014-10-03 22:52:48 +0200

    test for #33 in tests branch

--- tests/unit-tests/messages_test.py
+++ tests/unit-tests/messages_test.py
@@ -9,23 +9,25 @@ class StreamStub(object):
     def write(self, msg):
         self.observed_output += msg
 
+fixed_date = datetime(2000, 11, 2, 10, 23, 1, 556789)
+
 
 def test_no_properties():
     stream = StreamStub()
-    messages = TeamcityServiceMessages(output=stream, now=lambda: datetime.min)
+    messages = TeamcityServiceMessages(output=stream, now=lambda: fixed_date)
     messages.message('dummyMessage')
-    assert stream.observed_output == "\n##teamcity[dummyMessage timestamp='0001-01-01T00:00']\n"
+    assert stream.observed_output == "\n##teamcity[dummyMessage timestamp='2000-11-02T10:23:01.556']\n"
 
 
 def test_one_property():
     stream = StreamStub()
-    messages = TeamcityServiceMessages(output=stream, now=lambda: datetime.min)
+    messages = TeamcityServiceMessages(output=stream, now=lambda: fixed_date)
     messages.message('dummyMessage', fruit='apple')
-    assert stream.observed_output == "\n##teamcity[dummyMessage timestamp='0001-01-01T00:00' fruit='apple']\n"
+    assert stream.observed_output == "\n##teamcity[dummyMessage timestamp='2000-11-02T10:23:01.556' fruit='apple']\n"
 
 
 def test_three_properties():
     stream = StreamStub()
-    messages = TeamcityServiceMessages(output=stream, now=lambda: datetime.min)
+    messages = TeamcityServiceMessages(output=stream, now=lambda: fixed_date)
     messages.message('dummyMessage', fruit='apple', meat='steak', pie='raspberry')
-    assert stream.observed_output == "\n##teamcity[dummyMessage timestamp='0001-01-01T00:00' fruit='apple' meat='steak' pie='raspberry']\n"
+    assert stream.observed_output == "\n##teamcity[dummyMessage timestamp='2000-11-02T10:23:01.556' fruit='apple' meat='steak' pie='raspberry']\n"
