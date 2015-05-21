b559c575b692c03f2aad1c47b8080e18d9bae809
Lewis Coates <lewisx.coates@intel.com>
2015-05-21 16:16:03 -0700

    testing some experimental alterations

--- teamcity/messages.py
+++ teamcity/messages.py
@@ -52,7 +52,7 @@ class TeamcityServiceMessages(object):
             self.message('testFinished', name=testName, flowId=flowId)
 
     def testIgnored(self, testName, message='', flowId=None):
-        self.message('testIgnored', name=testName, message=message, flowId=flowId)
+        self.message('testIgnored', name=testName, message=message, flowId=flowId, details=message)
 
     def testFailed(self, testName, message='', details='', flowId=None):
         self.message('testFailed', name=testName, message=message, details=details, flowId=flowId)
