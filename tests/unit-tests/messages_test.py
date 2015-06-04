b531b080689d0781bfc1235b79b56017afd7e9d1
Marc Abramowitz <marc@marc-abramowitz.com>
2015-06-04 14:02:46 -0700

    Add test_unicode_to_sys_stdout_with_no_encoding
    This currently fails with:
    
        Traceback (most recent call last):
          File "/var/folders/gw/w0clrs515zx9x_55zgtpv4mm0000gp/T/tmp5eApc3", line 7, in <module>
            messages.message(bjork)
          File "/Users/marca/dev/git-repos/teamcity-python/teamcity/messages.py", line 30, in message
            self.output.write(message)
        UnicodeEncodeError: 'ascii' codec can't encode character u'\xf6' in position 14: ordinal not in range(128)

--- tests/unit-tests/messages_test.py
+++ tests/unit-tests/messages_test.py
@@ -1,5 +1,9 @@
 from teamcity.messages import TeamcityServiceMessages
 from datetime import datetime
+import os
+import sys
+import tempfile
+import textwrap
 
 
 class StreamStub(object):
@@ -42,3 +46,29 @@ def test_unicode():
     bjork = u'Bj\xf6rk Gu\xf0mundsd\xf3ttir'
     messages.message(bjork)
     assert stream.observed_output == u"\n##teamcity[%s timestamp='2000-11-02T10:23:01.556']\n" % bjork
+
+
+def test_unicode_to_sys_stdout_with_no_encoding():
+    with tempfile.NamedTemporaryFile() as tmpf:
+        tmpf.write(textwrap.dedent(r"""
+            from teamcity.messages import TeamcityServiceMessages
+
+            bjork = u'Bj\xf6rk Gu\xf0mundsd\xf3ttir'
+
+            messages = TeamcityServiceMessages()
+            messages.message(bjork)
+            print("hello")
+            """))
+        tmpf.flush()
+
+        ret = os.system("%s %s" % (sys.executable, tmpf.name))
+        assert ret == 0
+
+        # If we don't encode output, we could run into an error like this:
+        #
+        # Traceback (most recent call last):
+        #   File "/var/folders/gw/w0clrs515zx9x_55zgtpv4mm0000gp/T/tmp5eApc3", line 7, in <module>
+        #     messages.message(bjork)
+        #   File "/Users/marca/dev/git-repos/teamcity-python/teamcity/messages.py", line 30, in message
+        #     self.output.write(message)
+        # UnicodeEncodeError: 'ascii' codec can't encode character u'\xf6' in position 14: ordinal not in range(128)
