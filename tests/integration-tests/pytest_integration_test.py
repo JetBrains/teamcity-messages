cd6848c65a526a0f0379d6a3e2bd19ca4f2260ec
Leonid Shalupov <shalupov@diverse.org.ru>
2015-10-16 23:05:47 +0200

    fix 2.4, 3.2 compat

--- tests/integration-tests/pytest_integration_test.py
+++ tests/integration-tests/pytest_integration_test.py
@@ -73,30 +73,33 @@ def test_custom_test_items(venv):
         ])
 
 
-@pytest.mark.skipif("sys.version_info < (2, 6)", reason="requires Python 2.6+")
-@pytest.mark.parametrize("coverage_version, pytest_cov_version", [
-    ("==3.7.1", "==1.8.1"),
-    ("==4.0.1", "==2.2.0"),
-    # latest
-    ("", ""),
-])
-def test_coverage(venv, coverage_version, pytest_cov_version):
-    venv_with_coverage = virtual_environments.prepare_virtualenv(
-        venv.packages + (
-            "coverage" + coverage_version,
-            "pytest-cov" + pytest_cov_version))
+if sys.version_info >= (2, 6):
+    @pytest.mark.parametrize("coverage_version, pytest_cov_version", [
+        ("==3.7.1", "==1.8.1"),
+        ("==4.0.1", "==2.2.0"),
+        # latest
+        ("", ""),
+    ])
+    def test_coverage(venv, coverage_version, pytest_cov_version):
+        if coverage_version != "==3.7.1" and (3, 1) < sys.version_info < (3, 3):
+            pytest.skip("coverage >= 4.0 dropped support for Python 3.2")
 
-    output = run(venv_with_coverage, 'coverage_test', options="--cov coverage_test")
-    test_name = "tests.guinea-pigs.pytest.coverage_test.coverage_test.test_covered_func"
-    assert_service_messages(
-        output,
-        [
-            ServiceMessage('testStarted', {'name': test_name}),
-            ServiceMessage('testFinished', {'name': test_name}),
-            ServiceMessage('buildStatisticValue', {'key': 'CodeCoverageAbsLCovered', 'value': '9'}),
-            ServiceMessage('buildStatisticValue', {'key': 'CodeCoverageAbsLTotal', 'value': '13'}),
-            ServiceMessage('buildStatisticValue', {'key': 'CodeCoverageAbsLUncovered', 'value': '4'}),
-        ])
+        venv_with_coverage = virtual_environments.prepare_virtualenv(
+            venv.packages + (
+                "coverage" + coverage_version,
+                "pytest-cov" + pytest_cov_version))
+
+        output = run(venv_with_coverage, 'coverage_test', options="--cov coverage_test")
+        test_name = "tests.guinea-pigs.pytest.coverage_test.coverage_test.test_covered_func"
+        assert_service_messages(
+            output,
+            [
+                ServiceMessage('testStarted', {'name': test_name}),
+                ServiceMessage('testFinished', {'name': test_name}),
+                ServiceMessage('buildStatisticValue', {'key': 'CodeCoverageAbsLCovered', 'value': '9'}),
+                ServiceMessage('buildStatisticValue', {'key': 'CodeCoverageAbsLTotal', 'value': '13'}),
+                ServiceMessage('buildStatisticValue', {'key': 'CodeCoverageAbsLUncovered', 'value': '4'}),
+            ])
 
 
 def test_runtime_error(venv):
