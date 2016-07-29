251f3f4503ccc16caf83cf70ea68f17f6dc273f0
Marc Abramowitz <marc@marc-abramowitz.com>
2016-07-28 23:14:56 -0700

    Fix flake8 errors in flake8_plugin.py
    by adding `#noqa` comments.

--- teamcity/flake8_plugin.py
+++ teamcity/flake8_plugin.py
@@ -1,6 +1,6 @@
 try:
-    from flake8.formatting import base
+    from flake8.formatting import base  # noqa
 except ImportError:
-    from flake8_v2_plugin import *
+    from flake8_v2_plugin import *  # noqa
 else:
-    from flake8_v3_plugin import *
+    from flake8_v3_plugin import *  # noqa
