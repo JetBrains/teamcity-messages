38c48d17d1b479dbb17cc39b229edbcb1c539217
Leonid Shalupov <leonid@shalupov.com>
2017-01-02 19:14:39 +0100

    do not assert test name in test_discovery_errors test (fixes appveyor/travis compatibility)

--- tests/integration-tests/unittest_integration_test.py
+++ tests/integration-tests/unittest_integration_test.py
@@ -240,17 +240,12 @@ def test_discovery(venv):
 def test_discovery_errors(venv):
     output = run_directly(venv, 'discovery_errors.py')
 
-    if sys.version_info >= (3, 5):
-        test_name = "unittest.loader._FailedTest.testsimple"
-    else:
-        test_name = 'unittest.loader.ModuleImportFailure.testsimple'
-
     ms = assert_service_messages(
         output,
         [
-            ServiceMessage('testStarted', {'name': test_name, 'flowId': test_name}),
-            ServiceMessage('testFailed', {'name': test_name, 'message': 'Error', 'flowId': test_name}),
-            ServiceMessage('testFinished', {'name': test_name, 'flowId': test_name}),
+            ServiceMessage('testStarted', {}),
+            ServiceMessage('testFailed', {'message': 'Error'}),
+            ServiceMessage('testFinished', {}),
         ])
 
     assert ms[1].params['details'].index("ImportError") > 0
