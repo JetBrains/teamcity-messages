6c119f0a4e64baaf01650d156980881ebb5d4e96
Leonid Shalupov <leonid@shalupov.com>
2017-08-15 13:08:54 +0300

    travis: update pypy3 env

--- .travis-pypy3-virtualenv.sh
+++ .travis-pypy3-virtualenv.sh
@@ -3,8 +3,8 @@
 set -x -e -u
 
 VIRTENV=$1
-DIR=pypy3.3-5.5-alpha-20161013-linux_x86_64-portable
-wget -O $DIR.tar.bz2 https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3.3-5.5-alpha-20161013-linux_x86_64-portable.tar.bz2
+DIR=pypy3.5-5.8-beta-linux_x86_64-portable
+wget -O $DIR.tar.bz2 https://bitbucket.org/squeaky/portable-pypy/downloads/pypy3.5-5.8-1-beta-linux_x86_64-portable.tar.bz2
 tar xjf $DIR.tar.bz2
 cd $DIR
 
