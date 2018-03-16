68b8c9728567920dcff869f3c5d9cf8e8138d108
Ilya.Kazakevich <Ilya.Kazakevich@jetbrains.com>
2018-03-17 00:24:44 +0300

    Convert diff objects to text explicitly to prevent cases like https://youtrack.jetbrains.com/issue/PY-28893

--- tests/guinea-pigs/diff_assert.py
+++ tests/guinea-pigs/diff_assert.py
@@ -4,7 +4,7 @@ import unittest
 
 class FooTest(unittest.TestCase):
     def test_test(self):
-        self.assertEqual("spam", "eggs", "Fail")
+        self.assertEqual("spam", ("eggs",), "Fail")
 
 if __name__ == "__main__":
     from teamcity.unittestpy import TeamcityTestRunner
