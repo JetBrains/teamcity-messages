369102151ba98a69a976622bee93a8a8770ce368
Leonid Shalupov <leonid@shalupov.com>
2019-03-10 16:11:13 +0300

    setup.py: support 3.6 and 3.7

--- setup.py
+++ setup.py
@@ -73,6 +73,8 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Topic :: Software Development :: Testing'
     ],
     url="https://github.com/JetBrains/teamcity-messages",
