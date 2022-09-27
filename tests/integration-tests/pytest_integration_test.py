fe79ea6bb7883ffa3b8cad23c58b7ed842cd5a60
Mikhail Kidiankin <mikhail.kidiankin@jetbrains.com>
2022-09-27 15:28:42 +0200

    Skip test for pytest-flake8 1.1 for unsupported Python versions

--- tests/integration-tests/pytest_integration_test.py
+++ tests/integration-tests/pytest_integration_test.py
@@ -95,8 +95,9 @@ if (sys.version_info[0] == 2 and sys.version_info >= (2, 7)) or (sys.version_inf
         internal_test_pytest_flake8(venv_with_pylint, "tests.guinea-pigs.pytest.{}.FLAKE8")
 
 
+    @pytest.mark.skipif("sys.version_info < (3, 7)", reason="requires Python 3.7+")
     def test_pytest_flake8_v1_1(venv):
-        # Use flake8 < 4 as there is an issue in pytest-flake8 package:
+        # Use flake8 < 5 as there is an issue in pytest-flake8 package:
         # https://github.com/tholo/pytest-flake8/issues/87
         venv_with_pylint = virtual_environments.prepare_virtualenv(venv.packages + ("pytest-flake8",) + ("flake8==4.0.1",))
         internal_test_pytest_flake8(venv_with_pylint, "tests.guinea-pigs.pytest.{}.flake-8.FLAKE8")
